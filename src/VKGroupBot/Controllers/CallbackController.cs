5cb780502bc57cf154523ad656ab6bd982855eb0
Nikolay Savenko <savenkons@yandex.ru>
2021-02-11 12:28:52 +0300

    fixed double message bug

--- src/VKGroupBot/Controllers/CallbackController.cs
+++ src/VKGroupBot/Controllers/CallbackController.cs
@@ -2,72 +2,47 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using VkNet.Abstractions;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
-using VkNet.Utils;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using VkNet.Enums.SafetyEnums;
-using VkNet.Model.GroupUpdate;
 
-namespace VKGroupBot.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CallbackController : ControllerBase
-    {
-        /// <summary>
-        /// Конфигурация приложения
-        /// </summary>
-        private readonly IConfiguration _configuration;
+namespace VKGroupBot.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CallbackController : ControllerBase {
+		private readonly IConfiguration _configuration;
+		private readonly IVkApi _vkApi;
 
-        private readonly IVkApi _vkApi;
+		public CallbackController(IVkApi vkApi, IConfiguration configuration) {
+			_vkApi = vkApi;
+			_configuration = configuration;
+		}
 
-        public CallbackController(IVkApi vkApi, IConfiguration configuration)
-        {
-            _vkApi = vkApi;
-            _configuration = configuration;
-        }
+		[HttpPost]
+		public IActionResult Callback([FromBody] JsonElement body) {
+			var response = "ok";
+			var type = body.GetProperty("type").GetString();
 
-        [HttpPost]
-        public IActionResult Callback([FromBody] JsonElement body)
-        {
-             string response = "ok";
+			switch (type) {
+				case "confirmation":
+					response = Environment.GetEnvironmentVariable("vk_response");
+					break;
+				case "message_new":
+					var msgObject = body.GetProperty("object").GetProperty("message");
+					var message = JsonConvert.DeserializeObject<Message>(msgObject.ToString());
 
-             string type = body.GetProperty("type").GetString();
-             if (type == "confirmation")
-                 response = Environment.GetEnvironmentVariable("vk_response");
-             else if (type == "message_new")
-             {
-                 try
-                 {
-                     Message message = JsonConvert.DeserializeObject<Message>(body.GetProperty("object")
-                         .GetProperty("message")
-                         .ToString());
-                     string msg = message.Text;
-                     if (msg == "headers")
-                     {
-                         foreach (var key in Request.Headers.Keys)
-                         {
-                             msg += $"{key}: {Request.Headers[key].ToString()}\n";
-                         }
-                         response = "not_ok(";
-                     }
+					if (!Request.Headers.Keys.Contains("X-Retry-Counter"))
+						_vkApi.Messages.Send(new MessagesSendParams {
+							RandomId = new DateTime().Millisecond,
+							PeerId = message.PeerId.Value,
+							Message = message.Text
+						});
 
-                     _vkApi.Messages.Send(new MessagesSendParams
-                     {
-                         RandomId = new DateTime().Millisecond,
-                         PeerId = message.PeerId.Value,
-                         Message = msg
-                     });
-                 }
-                 finally
-                 {
-                     Console.WriteLine("looks like finally");
-                 }
-             }
-             return Ok(response);
-        }
-    }
+					break;
+			}
+
+			return Ok(response);
+		}
+	}
 }
